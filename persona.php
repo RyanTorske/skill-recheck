0bbb75dc31fcad5a8cca86c1c388a2cb967e3d62
Ryan Torske <rtorske@cnm.edu>
2020-04-28 17:37:21 -0600

    Trying to figure out why deployment is not functional

--- persona.php
+++ persona.php
@@ -2,3 +2,23 @@
 <h1>Ryan Torske</h1>
 
 <img src="ryan.jpg" alt="me">
+<ul>
+	<li><strong>Name</strong>: Ryan Torske</li>
+	<li><strong>Age</strong>: 25</li>
+	<li><strong>Gender</strong>: Male</li>
+	<li><strong>Technology</strong>: Iphone 6+, Dell G3 gaming laptop, Lenovo Legion Gaming Tower T730.</li>
+	<li><strong>Attitude</strong>:Very relaxed, enjoys the simple things. Loves working with computers, skilled in many aspects of computers.</li>
+	<li><strong>Occupation</strong>:Full Time Student, Part Time Programmer.</li>
+
+	<p><strong>Needs, Goals, Wanted Features</strong>
+	<ul>
+		<li>Needs to be able to listen to music to complete tasks.</li>
+		<li>Browse for new music, or game reviews with ease.</li>
+		<li>Wants to know that his website is being efficient and useful.</li>
+	</ul>
+	</p>
+	<p><strong>Frustrations and Issues</strong></p>
+	<ul>
+		<li>Wants less videos to show up that have nothing to do with his viewing habits.</li>
+	</ul>
+</ul>
